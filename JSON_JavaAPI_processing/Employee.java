5dd90511114448cb4715fdbb63363ace9033b95e
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-11-21 19:14:24 +0000

    Modify Employee class Adding getter's methods and constructors

--- JSON_JavaAPI_processing/Employee.java
+++ JSON_JavaAPI_processing/Employee.java
@@ -2,12 +2,11 @@ package readJSONData;
 
 import java.time.LocalDate;
 /**
-* Emplyee class
+* Employee class
 *
 * @author David Sajdl
 *
 */
-
 public class Employee {
 
 	private String firstName;
@@ -16,6 +15,22 @@ public class Employee {
 	private Integer empId;
 	private LocalDate hireDate;
 
+	public Employee(){
+		this.firstName = null;
+		this.lastName = null;
+		this.email = null;
+		this.empId = 0;
+		this.hireDate = null;
+	}
+
+	public Employee(Integer empId, String firstname, String lastname, String email, LocalDate hiredate){
+		this.firstName = firstname;
+		this.lastName = lastname;
+		this.email = email;
+		this.empId = empId;
+		this.hireDate = hiredate;
+	}
+	// setters
 	public void setEmpID(Integer id){
 		this.empId = id;
 	}
@@ -31,6 +46,23 @@ public class Employee {
 	public void setEmpHireDate(LocalDate hireDate){
 		this.hireDate = hireDate;
 	}
+	// getters
+	public Integer getEmployeeId(){
+		return this.empId;
+	}
+	public String getEmployeeFirstName(){
+		return this.firstName;
+	}
+	public String getEmployeeLastName(){
+		return this.lastName;
+	}
+	public String getEmployeeEmail(){
+		return this.email;
+	}
+	public LocalDate getEmployeeHireDate(){
+		return this.hireDate;
+	}
+
 	@Override
 	public String toString(){
 		return "Employee id: "+ this.empId + " | Full name: " + this.firstName + " " + this.lastName
