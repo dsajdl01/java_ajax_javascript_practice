2c8741ac1edee4624ba096058c735c8fdc36e559
dsajdl01 <davidsajdl@yahoo.co.uk>
2015-11-22 10:57:10 +0000

    modify members js file Adding validation process for the submitting form

--- members.js
+++ members.js
@@ -11,12 +11,13 @@ function memberData()
 	this.locat = "";
 }
 
-function getProcess(){
-	var isValid = true;
+function getProcess()
+{
+	var isValid = true; 
+	var dataVal = [true, true, true, true];
 	var memberdata = new Array(1);
-	alert("Data pass 11");
-	memberdata[0] = new memberData();
 
+	memberdata[0] = new memberData();
 	memberdata[0].username = document.getElementById("userName").value.trim();
 	memberdata[0].firstname = document.getElementById("firstName").value.trim();
 	memberdata[0].lastname = document.getElementById("lastName").value.trim();
@@ -24,30 +25,30 @@ function getProcess(){
 	var confEmail = document.getElementById("emailConf").value.trim();
 	memberdata[0].dob = document.getElementById("dob").value.trim();
 	var gender = document.getElementById("gender").value;
-	memberdata[0].loct = document.getElementById("location").value.trim(); 
+	memberdata[0].locat = document.getElementById("location").value.trim();
 	
-//	window.location.reload(true);
-
-	// validate First name
-	if(getValid(memberdata[0].username)) {
-		//getFocusOnField(memberdata, 0);
-		isValid = false;
-	} 
-
+	clearErrorMessages();
 	
 	// vaidata UserName
 	if(getValid(memberdata[0].firstname)) {
-		//getFocusOnField(memberdata, 1);
 		isValid = false;
+		dataVal[0] = false;
 	}
+
+	// validate First name
+	if(getValid(memberdata[0].username)) {
+		isValid = false;
+		dataVal[1] = false;
+	} 
 	// validate email
 	if(validateEmail(memberdata[0].email)){
-		alert(memberdata[0].email + " == " + confEmail + " is: " + (memberdata[0].email != confEmail));
 		if(memberdata[0].email != confEmail){
 			isValid = false;
+			dataVal[3] = false;
 		}
 	} else {
 		isValid = false;
+		dataVal[2] = false;
 	}
 	// checking genda
 	if (gender == "Female") {
@@ -56,34 +57,109 @@ function getProcess(){
 		memberdata[0].gnd = "M";
 	} else {
 		memberdata[0].gnd = null;
-	}
+	} 
 
 	if(isValid) {
 		// send data to database
+		window.location.reload(true);
 		alert("Data: " + memberdata[0].firstname  + " is successfully added" );
+		
 	} 
+	else 
+	{
+		var getFocus = true;
+		setValueBack(memberdata);
+
+		for(var i = 0; i < dataVal.length; i++)
+		{
+			if(dataVal[i] == false)
+			{
+				displayErrorMessage(getFocus, i);
+				getFocus = false;
+			}
+
+		}
+	}
+
 }
 
-function validateEmail(inputText) {  
+function  setValueBack(memberdata)
+{
+	document.getElementById("firstName").value = memberdata[0].firstname;
+	document.getElementById("userName").value = memberdata[0].username;
+	document.getElementById("lastName").value = memberdata[0].lastname;
+	document.getElementById("email").value = memberdata[0].email;
+	document.getElementById("emailConf").value = "";
+	document.getElementById("dob").value = memberdata[0].dob;
+	document.getElementById("location").value = memberdata[0].locat;
+}
+
+function validateEmail(inputText)
+{  
 	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
-	if(inputText.match(mailformat)) {
+	if(inputText.match(mailformat))
+	{
 		return true;  
 	}     
 	return false;
 } 
+function clearErrorMessages()
+{
+	document.getElementById("firstNameId").innerHTML = "";
+	document.getElementById("userNameId").innerHTML = "";
+	document.getElementById("emailId").innerHTML = "";
+	document.getElementById("emailConfId").innerHTML = "";
+}
 
-function getFocusOnField(usersData, f){
-	if(f == 0){
-		document.getElementById("EntryUserName").value = "";
-		document.getElementById("EntryUserName").focus();
-		alert("Invalid user name. Please enter user name." );
+function displayErrorMessage(focus, index)
+{
+	if(index == 0)
+	{
+		document.getElementById("firstName").value = "";
+		document.getElementById("firstNameId").innerHTML = "* Entry First Name Please.";
+		if(focus)
+		{
+			document.getElementById("firstName").focus();
+			alert("Invalid First name. Please enter First name. \nIt must be greater than 2 characters" );
+		}
+	}
+	if(index == 1)
+	{
+		document.getElementById("userName").value = "";
+		document.getElementById("userNameId").innerHTML = "* Entry User Name Please.";
+		if(focus)
+		{
+			document.getElementById("userName").focus();
+			alert("Invalid user name. Please enter user name. \nIt must be greater than 2 characters" );
+		}
+	}
+	if(index == 2)
+	{
+		document.getElementById("email").value = "";
+		document.getElementById("emailConf").value = "";
+		document.getElementById("emailId").innerHTML = "* Entry E-mail Address Please.";
+		if(focus)
+		{
+			document.getElementById("email").focus();
+			alert("Invalid E-mail Address. Please enter valied E-mail Address.");
+		}
+	}
+	if(index == 3)
+	{
+		document.getElementById("emailConf").value = "";
+		document.getElementById("emailConfId").innerHTML = "* Entry Same E-mail! Please.";
+		if(focus)
+		{
+			document.getElementById("emailConf").focus();
+			alert("E-mail Address Does Not Match. \nPlease Entry the Same E-mail Address.");
+		}
 	}
 }
 
-function getValid(tagsVal){
+function getValid(tagsVal)
+{
     	var tg = tagsVal.replace(/[, ]+/g, " ").trim();
     	tg = tg.replace(/[, ]+/g, " ").trim();
-    	//alert(tg.length <= 1);
     	return tg.length <= 2;
 }
 
